472fb39a750e643b626ae2001ff8cca1b713e101
derTurke <gurhan061@gmail.com>
2021-12-29 19:54:57 +0300

    Initial Commit

---
+++ eRestaurant-Tez/eRestaurant_TezApp.swift
@@ -0,0 +1,17 @@
+//
+//  eRestaurant_TezApp.swift
+//  eRestaurant-Tez
+//
+//  Created by GÜRHAN YUVARLAK on 29.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct eRestaurant_TezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
