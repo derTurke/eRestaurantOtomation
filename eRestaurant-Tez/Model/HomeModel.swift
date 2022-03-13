e4c94dae8f777c5b0b77e097b8cdada8c18d50be
derTurke <gurhan061@gmail.com>
2022-03-13 21:43:40 +0300

    Last 1

---
+++ eRestaurant-Tez/Model/HomeModel.swift
@@ -0,0 +1,15 @@
+//
+//  HomeModel.swift
+//  eRestaurant-Tez
+//
+//  Created by GÜRHAN YUVARLAK on 13.03.2022.
+//
+
+import SwiftUI
+
+struct HomeProductModel: Codable {
+    var id : Int
+    var image : String
+    var name : String
+    var price : Double
+}
